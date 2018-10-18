fb13310e3859545d48f835bfee79480149feb750
Kartikay Sharma <kartikay@uranus.cs.colostate.edu>
2018-10-17 18:11:48 -0600

    storage_constants.py to gitignore

--- activations/data_utils/io/storage_constants.py
+++
@@ -1,2 +0,0 @@
-DATA_SETS_lOCATION = "/home/kartik/playground/code/machine_learning/datasets/"
-RESULTS_LOCATION = "/home/kartik/playground/code/machine_learning/experiment_results/activations/"
--- activations/temp.py
+++ activations/temp.py
@@ -14,6 +14,7 @@ from reveal.structures.loss_experiment_structure import *
 from reveal.graphics.plot.loss_plotter import *
 
 import json
+import time
 
 dataPath = sc.DATA_SETS_lOCATION+"auto_mpg/auto-mpg.data"
 
@@ -21,17 +22,17 @@ x, t, x_names, t_names = mpg.make_mpg_data(dataPath)
 
 lc = rf.LossComparision()
 
-lc.params['repetitions'] = 3
-lc.params['epochs'] = 10
+lc.params['repetitions'] = 5
+lc.params['epochs'] = 30
 lc.params['batch_size'] = 20
 lc.params['problem_type'] = consts.PROBLEM_TYPE_REGRESSION
 lc.params['network_type'] = consts.NETWORK_TYPE_FEEDFORWARD
-lc.params['net_structures'] = [[4, 5,2,2,2], [4,5,6,4]]
+lc.params['net_structures'] = [[4000, 5000,4000,4000,4000]]
 lc.params['activation_fs'] = ["relu", "tanh"]
 lc.params['loss_function'] = nn.MSELoss()
 lc.params['optimizer'] = []
-lc.params['use_cuda'] = True
-lc.params['verbosity'] = consts.VERBOSE_DETAILED_INFO
+lc.params['use_cuda'] = False
+lc.params['verbosity'] = consts.VERBOSE_MED_INFO
 
 results = lc.compare_loss(x, t)
 
@@ -46,7 +47,7 @@ lp.params["y_axis_label"] = "MSE"
 # fig, ax = lp.get_plots(lp.TYPE_MEAN_ACTIVATIONS_TOGETHER, alpha = 0.4, show = True)
 # fig, ax = lp.get_plots(lp.TYPE_MEAN_ACTIVATIONS_SEPARATE, alpha = 0.4, show = True)
 #fig.show()
-
+s_t = time.time()
 #print(results.read_me)
 # results.save("mean.pickle")
 #
